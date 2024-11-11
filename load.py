e5dd2af0a887f9961dd246519ec6609b8f775107
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-11-11 16:49:38 +0530

    Add data loading functionality with OpenAI embeddings and MongoDB Atlas integration

---
+++ load.py
@@ -0,0 +1,28 @@
+# Load Data #
+
+# Import Packages
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain.document_loaders import DirectoryLoader
+from mongo import collection
+import env as e
+
+# Get Env Variables
+openai_api_key = e.openai_api_key  # OpenAI API Key
+
+# Loader Params
+path = "./data"
+glob = "./*.txt"
+progress = True
+
+# Directory Loader
+loader = DirectoryLoader(path=path, glob=glob, show_progress=progress)
+
+# Load Data
+data = loader.load()
+
+# Generate Embeddings
+embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+
+# Vector Store
+vectorStore = MongoDBAtlasVectorSearch.from_documents(data, embeddings, collection)
