6b76469f07590c87d3234f1454fc1f9e5cca1ec6
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-11-11 16:49:33 +0530

    Add query data functionality using OpenAI and MongoDB Atlas

---
+++ query.py
@@ -0,0 +1,45 @@
+# Query Data #
+
+# Import Packages
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from mongo import collection
+import env as e
+
+# Variables
+model = "gpt-4o-mini"
+
+# Get Env Variables
+openai_api_key = e.openai_api_key  # OpenAI API Key
+
+# Embeddings
+embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+
+# Vector Store
+vectorStore = MongoDBAtlasVectorSearch(collection, embeddings)
+
+
+# Query Data Function
+def query_data(query):
+    # Similarity Search For Query
+    docs = vectorStore.similarity_search(query, k=1)
+
+    # First Doc Page Content
+    data = docs[0].page_content
+
+    # OpenAI LLM Instance
+    llm = OpenAI(api_key=openai_api_key, model=model, temperature=0)
+
+    # Retriever Instance
+    retriever = vectorStore.as_retriever()
+
+    # QA Chain Instance
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+    # LLM Answer
+    answer = ga.run(query)
+
+    # Return Data and Answer
+    return data, answer
