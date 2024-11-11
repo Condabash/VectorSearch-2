60331224f351b1aa7f82578c5ff476ce85aa2b6a
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-11-11 16:49:50 +0530

    Add Gradio app interface for question answering using Atlas Vector Search

---
+++ app.py
@@ -0,0 +1,34 @@
+# App #
+
+# Import Packages
+from gradio.themes.base import Base
+from query import query_data
+import gradio as gr
+
+# App Interface
+
+# Title
+with gr.Blocks(theme=Base(), title="QnA App") as demo:
+    # Header
+    gr.Markdown("# Question Answering App Using Atlas Vector Search + RAG Architecture")
+    # Textbox and Button
+    textbox = gr.Textbox(label="Enter Your Question:")
+    with gr.Row():
+        button = gr.Button("Submit", variant="primary")
+    with gr.Column():
+        output1 = gr.Textbox(
+            lines=1,
+            max_lines=10,
+            label="Output Using Atlas Vector Search: (Returns Page Content)",
+        )
+        output2 = gr.Textbox(
+            lines=1,
+            max_lines=10,
+            label="Output Using Chaining: Atlas Vector Search -> LangChain RetrieverQA -> OpenAI LLM: (Returns Answer)",
+        )
+
+    # Button Click Event Handler Function Call
+    button.click(query_data, textbox, outputs=[output1, output2])
+
+# Launch The App
+demo.launch()
